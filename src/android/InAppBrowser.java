53c8badb24d64b8ef6b89eb1f4b8c8a8df3f2a76
hpfranck <hpfranck@gmail.com>
2021-02-28 10:17:23 -0300

    Add files via upload

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -18,6 +18,8 @@
 */
 package org.apache.cordova.inappbrowser;
 
+import android.Manifest;
+import android.app.Activity;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Context;
