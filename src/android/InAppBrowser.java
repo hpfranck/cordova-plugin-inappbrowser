096e3db7c41f86e935b50f1fbcb5013b986a6e04
hpfranck <hpfranck@gmail.com>
2021-02-28 10:28:09 -0300

    Add files via upload

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -964,7 +964,7 @@ public class InAppBrowser extends CordovaPlugin {
                                 photoFile = createImageFile();
                                 takePictureIntent.putExtra("PhotoPath", mCM);
                             }catch(IOException ex){
-                                Log.e(LOG_TAG, "Image file creation failed", ex);
+                                LOG.e(LOG_TAG, "Image file creation failed", ex);
                             }
                             if(photoFile != null){
                                 mCM = "file:" + photoFile.getAbsolutePath();
